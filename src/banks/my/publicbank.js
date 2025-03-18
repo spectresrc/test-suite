a76c14dd35f4e116c78f18e6a2d5a7a0c0c4bee5
Gan Chu Hang <hgun77@gmail.com>
2025-03-18 13:29:35 +0800

    feat: add Public Bank automation script using Puppeteer; update start script in package.json

---
+++ src/banks/my/publicbank.js
@@ -0,0 +1,135 @@
+import _ from 'lodash';
+import { DateTime } from 'luxon';
+import puppeteer from 'puppeteer';
+import * as CONFIG from '../../config/index.js';
+
+const username = 'hgun77';
+const password = 'On$4669668';
+const recipient = {
+  bank: 'rhb',
+  account: '21212500188157',
+};
+const sender = {
+  account: '123123123'
+};
+
+const bankMappings = {
+
+};
+
+async function main() {
+  console.log(`🕛`, `${DateTime.now().toSQL()}`);
+  console.log(`🔆 Start Troubleshoot`);
+
+  if (CONFIG.puppeteer.proxy) {
+    console.log(`😎 PROXY LOGIN detected:`, CONFIG.puppeteer.proxy.host);
+    CONFIG.puppeteer.option.args.push(`--proxy-server=${CONFIG.puppeteer.proxy.host}`);
+  }
+
+  const browser = await puppeteer.launch(CONFIG.puppeteer.option);
+  const pages = await browser.pages();
+  const page = pages.shift();
+
+  const version = await browser.version();
+  console.log(`😎 Chrome version:`, version);
+  console.log(`😎 user_agent:`, CONFIG.puppeteer.user_agent);
+  console.log(`😎 CONFIG.puppeteer.option:`, CONFIG.puppeteer.option);
+
+  page.setUserAgent(CONFIG.puppeteer.user_agent);
+  // await page.setViewport({ width: 1366, height: 768, deviceScaleFactor: 1 });
+  page.on("dialog", (dialog) => dialog.accept());
+
+  if (CONFIG.puppeteer.proxy) {
+    const [username, password] = CONFIG.puppeteer.proxy.auth.split(':');
+    await page.authenticate({ username, password });
+    console.log(`😎 PROXY LOGIN authenticated:`, CONFIG.puppeteer.proxy.host, { username, password });
+  }
+
+  // get current ip address
+  const ip = await page.goto('https://checkip.amazonaws.com/', { waitUntil: ['networkidle0'], timeout: 5_000 });
+  console.log(`😎 IP Address:`, (await ip.text()).trim());
+
+  // await page.goto('https://api.ipify.org/', { timeout: 60000, waitUntil: ['networkidle0', 'networkidle2'] });
+  // await page.goto('https://scrapfly.io/web-scraping-tools/http2-fingerprint', { timeout: 60000, waitUntil: ['networkidle0', 'networkidle2'] });
+  // await page.goto('https://scrapfly.io/web-scraping-tools/ja3-fingerprint', { timeout: 60000, waitUntil: ['networkidle0', 'networkidle2'] });
+
+  const PUBLICBANK_URL = 'https://www2.pbebank.com/pbemain.html';
+  const PUBLICBANK_LOGIN_URL = 'https://www2.pbebank.com/eaijct/Public_Bank/user_cnpy';
+
+  await page.goto(PUBLICBANK_URL, { waitUntil: 'networkidle0' });
+  await page.goto(PUBLICBANK_LOGIN_URL, { waitUntil: 'networkidle0' });
+  await page.waitForSelector('#NextBtn', { timeout: 5_000 });
+  await page.type('input[name=tempusername]', username);
+  await page.click('#NextBtn');
+
+  // await page.waitForSelector("#phrase_image");
+  await page.waitForSelector('input[type="radio"][name="passcred"]', { timeout: 5_000 });
+
+  // await page.evaluate(() => clickLabelAndChange('YES'));
+  await page.click('input[type="radio"][name="passcred"][value="YES"]');
+
+  await page.type('input[name=password]', password);
+  await page.click('#SubmitBtn');
+
+  await new Promise(_ => setTimeout(_, 2_000));
+
+  try {
+    await page.waitForSelector('#DuplicateLoginDialog', { timeout: 3_000 });
+    console.log('Duplicate Login Dialog Found');
+    await page.goto('https://www2.pbebank.com/myIBK/apppbb/servlet/BxxxServlet?RDOName=BxxxAuth&MethodName=duplicateLogin');
+
+  } catch (error) {
+    console.log('No Duplicate Login Dialog');
+  };
+
+  // javascript:location.href='https://www2.pbebank.com/myIBK/apppbb/servlet/BxxxServlet?RDOName=BxxxAuth&MethodName=duplicateLogin'
+  // https://www2.pbebank.com/myIBK/apppbb/servlet/BxxxServlet?RDOName=BxxxAuth&MethodName=duplicateLogin
+
+  await new Promise(_ => setTimeout(_, 5_000));
+
+  {
+    console.log('🔆 Click on Account');
+    // last resort to use page.evaluate
+    await page.evaluate(() => {
+      document.querySelectorAll('.tiles a')[0].click();
+    });
+  }
+
+  await new Promise(_ => setTimeout(_, 2_000));
+
+  {
+    console.log('🔆 Click on Fund Transfer');
+    await page.waitForSelector('.top-menu a:nth-child(3)', { timeout: 5_000 });
+    await page.click('.top-menu a:nth-child(3)');
+  }
+
+  await new Promise(_ => setTimeout(_, 2_000));
+
+  {
+    console.log('🔆 Click on Other Bank Account');
+    await page.waitForSelector('ul.page-sidebar-menu>li', { timeout: 2_000 });
+    const [link] = await page.$$x("//a[.//span[@class='title' and normalize-space()='Other Bank Account']]");
+    if (link) await link.click();
+  }
+
+  await new Promise(_ => setTimeout(_, 2_000));
+
+  {
+    console.log('🔆 Click on DuitNow Transfer');
+    await page.waitForSelector('ul.page-sidebar-menu>li', { timeout: 2_000 });
+    const [link] = await page.$$x("//a[.//span[@class='title' and normalize-space()='DuitNow Transfer']]");
+    if (link) await link.click();
+  }
+
+  await new Promise(_ => setTimeout(_, 2_000));
+
+  {
+    console.log('🔆 Click on To Other Account');
+    await page.waitForSelector('ul.page-sidebar-menu>li', { timeout: 2_000 });
+    const [link] = await page.$$x("//li[@class='open']//ul[@class='sub-menu']//a[normalize-space()='To Other Account']");
+    if (link) await link.click();
+  }
+
+}
+
+main();
